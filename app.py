1a108cfb6dc54710690496ee49ce5bf108e84867
DrRoad <wei.liu@ghd.com>
2024-01-24 16:12:20 +1300

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,32 @@
+import ee
+import json
 import streamlit as st
-import geemap.foliumap as geemap
+from google.oauth2 import service_account
+from ee import oauth
+import geemap
 
-st.set_page_config(layout="wide")
+def ee_initialize(force_use_service_account=False):
+    if force_use_service_account or "json_data" in st.secrets:
+        json_credentials = st.secrets["json_data"]
+        credentials_dict = json.loads(json_credentials)
 
-# Customize the sidebar
-markdown = """
-Web App URL: <https://geemap.streamlit.app>
-"""
+        if 'client_email' not in credentials_dict:
+            raise ValueError("Service account info is missing 'client_email' field.")
 
-st.sidebar.title("About")
-st.sidebar.info(markdown)
-logo = "https://i.imgur.com/UbOXYAU.png"
-st.sidebar.image(logo)
+        credentials = service_account.Credentials.from_service_account_info(
+            credentials_dict, scopes=oauth.SCOPES
+        )
+        ee.Initialize(credentials)
+    else:
+        ee.Initialize()
 
-# Customize page title
-st.title("Earth Engine Web App")
+# Initialize GEE
+ee_initialize(force_use_service_account=True)
 
-st.markdown(
-    """
-    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [geemap](https://geemap.org). It is an open-source project and you are very welcome to contribute to the [GitHub repository](https://github.com/giswqs/geemap-apps).
-    """
-)
+# Streamlit app layout
+st.title('GEE-API-Streamlit Integration')
 
-st.header("Instructions")
-
-markdown = """
-1. For the [GitHub repository](https://github.com/giswqs/geemap-apps) or [use it as a template](https://github.com/new?template_name=geemap-apps&template_owner=giswqs) for your own project.
-2. Customize the sidebar by changing the sidebar text and logo in each Python files.
-3. Find your favorite emoji from https://emojipedia.org.
-4. Add a new app to the `pages/` directory with an emoji in the file name, e.g., `1_🚀_Chart.py`.
-"""
-
-st.markdown(markdown)
-
-m = geemap.Map()
-m.add_basemap("OpenTopoMap")
-m.to_streamlit(height=500)
+# Create and display a map
+Map = geemap.Map(center=[40, -100], zoom=4)
+Map.add_basemap('SATELLITE')
+Map.to_streamlit(height=300)
